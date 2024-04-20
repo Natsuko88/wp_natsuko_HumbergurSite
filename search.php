62b1720908d13e83632a72091dae9ceaa1e402ca
AyumiYamamura-cloud <138938774+AyumiYamamura-cloud@users.noreply.github.com>
2024-04-20 21:32:14 +0900

    ファイル更新

--- search.php
+++ search.php
@@ -7,15 +7,10 @@
                         </h2>
                         <?php echo get_search_query(); ?>
                     </div>
-                    <?php
-                      $cat = get_the_category();
-                      $catname = $cat[0];
-                      $cat_description = $cat->category_description;
-                    ?>
                     <article class="p-article--archive">
-                        <h3 class="c-sub-title"><?php echo esc_html($cat->name); ?></h3>
+                        <h3 class="c-sub-title">小見出しが入ります</h3>
                         <p class="c-sub-article">
-                            <?php echo esc_html($cat_description); ?>
+                        テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。
                         </p>
                         <section class="p-content--archive">
                             <!--メインループ検索結果出力-->
@@ -51,105 +46,12 @@
                         
                         <?php endif; ?>
                 
-                        
-                            <section class="p-card--archive">
-                                <img class="c-article-img" src="img/archive-card.jpg" alt="ダブルチーズバーガーの画像">
-                                <article class="p-card--archive--article">
-                                    <h3 class="c-content-title c-content-title--archive">
-                                        見出しが入ります
-                                    </h3>
-                                    <h4 class="c-content-title c-content-title--sub">
-                                        小見出しが入ります
-                                    </h4>
-                                    <p class="c-card-article c-card-article--archive">
-                                        テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。
-                                    </p>
-                                    <button class="c-button c-button--detail">
-                                        詳しく見る
-                                    </button>
-                                </article>
-                            </section>
-                        
-                            <section class="p-card--archive">
-                                <img class="c-article-img" src="img/archive-card.jpg" alt="スペシャルチーズバーガーの画像">
-                                <article class="p-card--archive--article">
-                                    <h3 class="c-content-title c-content-title--archive">
-                                        見出しが入ります
-                                    </h3>
-                                    <h4 class="c-content-title c-content-title--sub">
-                                        小見出しが入ります
-                                    </h4>
-                                    <p class="c-card-article c-card-article--archive">
-                                        テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。
-                                    </p>
-                                    <button class="c-button c-button--detail">
-                                        詳しく見る
-                                    </button>
-                                </article>
-                            </section>
-                            <section class="p-card--archive">
-                                <img class="c-article-img" src="img/archive-card.jpg" alt="スペシャルチーズバーガーの画像">
-                                <article class="p-card--archive--article">
-                                    <h3 class="c-content-title c-content-title--archive">
-                                        見出しが入ります
-                                    </h3>
-                                    <h4 class="c-content-title c-content-title--sub">
-                                        小見出しが入ります
-                                    </h4>
-                                    <p class="c-card-article c-card-article--archive">
-                                        テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。
-                                    </p>
-                                    <button class="c-button c-button--detail">
-                                        詳しく見る
-                                    </button>
-                                </article>
-                            </section>
-                            <section class="p-card--archive">
-                                <img class="c-article-img" src="img/archive-card.jpg" alt="スペシャルチーズバーガーの画像">
-                                <article class="p-card--archive--article">
-                                    <h3 class="c-content-title c-content-title--archive">
-                                        見出しが入ります
-                                    </h3>
-                                    <h4 class="c-content-title c-content-title--sub">
-                                        小見出しが入ります
-                                    </h4>
-                                    <p class="c-card-article c-card-article--archive">
-                                        テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。
-                                    </p>
-                                    <button class="c-button c-button--detail">
-                                        詳しく見る
-                                    </button>
-                                </article>
-                            </section>
                         </section>
                     </article>      
                 </div> 
                 <section class="p-pagenation">
                     <!--ページネーション-->
                     <?php wp_pagenavi(); ?>
-
-                    <p class="c-pagenation-head">page 1/10</p>
-                    <button class="c-button c-button--pagenation">
-                        <img src="img/pagenation.png" alt="ページングボタン">
-                        <p class="c-button c-button--page-text">前へ</p>
-                    </button>
-                    
-                    <ul class="p-pagenation-list">
-                        <li class="c-pagenation u-pagenation">1<li>
-                        <li class="c-pagenation">2</li>
-                        <li class="c-pagenation">3</li>
-                        <li class="c-pagenation">4</li>
-                        <li class="c-pagenation">5</li>
-                        <li class="c-pagenation">6</li>
-                        <li class="c-pagenation">7</li>
-                        <li class="c-pagenation">8</li>
-                        <li class="c-pagenation">9</li>
-                    </ul>
-                    
-                    <button class="c-button c-button--pagenation u-button--pagenation">
-                        <p class="c-button c-button--page-text">次へ</p>
-                        <img src="img/pagenation.png" alt="ページングボタン">
-                    </button>
                 </section>
                 
             </main>
